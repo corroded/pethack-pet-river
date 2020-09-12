ce62a8d45d2431203b93a44abe3947a2d32b1e55
Eumir Gaspar <eumirgaspar@gmail.com>
2020-09-13 05:09:25 +0800

    Added styles and more polish on show

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
 class Pet < ApplicationRecord
+  belongs_to :human, class_name: 'User', foreign_key: 'human_id'
+
+  delegate :phone_number, to: :human, prefix: false, allow_nil: false
 end
