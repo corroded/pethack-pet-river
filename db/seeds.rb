d9830cd7edf96152fabb6a1754600b33eb8ade68
Eumir Gaspar <eumirgaspar@gmail.com>
2020-09-13 06:43:23 +0800

    Added some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+human = User.create first_name: "Yolomir", last_name: "Swagamir", phone_number: "+614443333", email: "yolomir@example.com", password: nil
+Pet.create!(name: "Swag", age: "12", vaccinated: false, registered: false,  url: "uhf321", bounty: 50, human_id: human.id)
