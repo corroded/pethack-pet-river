4222962f889b837e90e7406785a2b5e2c7bb5463
Eumir Gaspar <eumirgaspar@gmail.com>
2020-09-13 06:11:24 +0800

    Generate gmaps url with SMS

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -4,6 +4,16 @@ class NotificationsController < ApplicationController
   def create
     pet = Pet.find_by(url: params[:id])
 
-    MessageSender.send_message("Someone has found #{pet.name}! Click here to see where they are!")
+    MessageSender.send_message("Someone has found #{pet.name} at #{generate_map_url}")
+  end
+
+  private
+
+  def permitted_params
+    params.permit(:id, :lat, :lon)
+  end
+
+  def generate_map_url
+    "https://www.google.com/maps/search/?api=1&query=#{permitted_params[:lat]},#{permitted_params[:lon]}"
   end
 end
