5c3620a254e7ca69d86497acdae6cf0672a1c264
Eumir Gaspar <eumirgaspar@gmail.com>
2020-09-13 11:35:49 +0800

    Added editing

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,4 +6,19 @@ class PetsController < ApplicationController
   def edit
     @pet = Pet.find(params[:id])
   end
+
+  def update
+    @pet = Pet.find(params[:id])
+
+    if @pet.update!(pet_params)
+      flash[:notice] = 'Pet profile updated!'
+      redirect_to pet_path(@pet.url)
+    end
+  end
+
+  private
+
+  def pet_params
+    params.require(:pet).permit(:name, :bounty, :description)
+  end
 end
