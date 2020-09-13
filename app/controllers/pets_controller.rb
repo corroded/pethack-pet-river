2f6dcc331fa9fdb62df41e14ab286e3ac3c9f180
Eumir Gaspar <eumirgaspar@gmail.com>
2020-09-13 11:13:21 +0800

    Added edit page + styles

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,4 +2,8 @@ class PetsController < ApplicationController
   def show
     @pet = Pet.find_by(url: params[:id])
   end
+
+  def edit
+    @pet = Pet.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :pets, only: %i[show]
+  resources :pets, only: %i[show edit update]
   resource :notify, only: %i[create], controller: :notifications
 end
